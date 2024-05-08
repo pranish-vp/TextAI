5a36acbea3b3de8b939fab5463d9db32869ec412
Pranish VP <pranishvp@gmail.com>
2024-05-08 10:27:21 +0530

    modified

--- Frontend/src/components/config.js
+++ Frontend/src/components/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
+console.log(process.env.REACT_APP_apiKey)
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
